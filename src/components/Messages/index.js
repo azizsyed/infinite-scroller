ba832e00e22adb6637a4c3d9a1975b7f3d495182
Aziz Syed <aziz.syed@nike.com>
2017-10-22 14:24:43 -0700

    Make the header "fixed"

--- src/components/Messages/index.js
+++ src/components/Messages/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import Message from '../Message';
+import styled from 'styled-components';
+import { HEADER_HEIGHT } from '../../constants/styles';
 
-export default () => (
-  <div>
+const MessageWrapper = styled.div`
+  padding-top: ${HEADER_HEIGHT};
+`;
+
+export const Messages = ({ messages }) => (
+  <MessageWrapper>
     <h2>===== MESSAGES START =====</h2>
-    <Message />
-    <Message />
-    <Message />
+    {
+      messages.map(message => <Message key={message.id} onSelect={(e) => {
+        e.preventDefault;
+        console.log('select: ', e);
+        return false;
+      }} onDelete={(e) => {
+        e.preventDefault;
+        console.log('delete: ', e);
+        return false;
+      }} message={message} />)
+    }
     <h2>===== MESSAGES END =====</h2>
-  </div>
+  </MessageWrapper>
 );
+
+export default Messages;
